6630ab5ad0654d3c0c3cb13197d7beaecf96caa2
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-10-18 09:36:04 +0530

    removed experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import About from '../components/About';
 import Skills from '../components/Skills';
-import Experience from '../components/Experience';
+// import Experience from '../components/Experience';
 import Projects from '../components/Projects';
 import SocialLinks from '../components/SocialLinks';
 import Education from '../components/Education';
@@ -22,7 +22,7 @@ export default function Home() {
           </div>
           <About />
           <Skills />
-          <Experience />
+          {/* <Experience /> */}
           <Education />
           <Projects />
         </main>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 const Navigation: React.FC = () => {
   return (
     <nav className="flex space-x-4 items-center">
-      <Link href="#about" className="text-gray-400 hover:text-white">About</Link>
-      <Link href="#skills" className="text-gray-400 hover:text-white">Skills</Link>
-      <Link href="#experience" className="text-gray-400 hover:text-white">Experience</Link>
-      <Link href="#education" className="text-gray-400 hover:text-white">Education</Link>
-      <Link href="#projects" className="text-gray-400 hover:text-white">Projects</Link>
+      <Link href="#about" className="text-gray-400 hover:text-white">
+        About
+      </Link>
+      <Link href="#skills" className="text-gray-400 hover:text-white">
+        Skills
+      </Link>
+      {/* <Link href="#experience" className="text-gray-400 hover:text-white">Experience</Link> */}
+      <Link href="#education" className="text-gray-400 hover:text-white">
+        Education
+      </Link>
+      <Link href="#projects" className="text-gray-400 hover:text-white">
+        Projects
+      </Link>
     </nav>
   );
 };
