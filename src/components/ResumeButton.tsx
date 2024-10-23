2eaf855c7935a0767586a5138c0b5350312a91ec
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-10-23 11:07:57 +0530

    conveted resume to latex

--- src/components/ResumeButton.tsx
+++ src/components/ResumeButton.tsx
@@ -7,7 +7,7 @@ import { FaFileDownload } from "react-icons/fa";
 const ResumeButton: React.FC = () => {
   return (
     <a
-      href="/Piyush Choudhari Resume.pdf"
+      href="/Piyush_Choudhari_Resume.pdf"
       download
       className="flex items-center bg-[#111111] hover:bg-white text-white hover:text-[#111111] px-3 py-1 rounded transition duration-300 mr-4 border border-white"
     >
