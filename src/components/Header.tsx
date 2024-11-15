db7427a1b4a544511c880b8a1bded0b4e8b2ea5d
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-11-15 10:21:45 +0530

    swirl effect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import Image from "next/legacy/image";
+// import Image from "next/legacy/image";
+import GlowingProfile from "./GlowingProfile";
 
 const Header: React.FC = () => {
   return (
     <header>
-      <div className="mb-4 relative w-[150px] h-[150px] overflow-hidden rounded-full">
-        <Image
-          src="/linkedIn_profile_pic (2).png" // Replace with the actual path to your image
-          alt="Piyush Choudhari"
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
+      <GlowingProfile />
       <h1 className="text-4xl font-bold mb-2">Piyush Choudhari</h1>
       <h2 className="text-xl text-gray-400 mb-4">Developer & Data Analyst</h2>
       <h3 className="text-lg text-gray-400 mb-4">📍 Pune, India 🇮🇳</h3>
