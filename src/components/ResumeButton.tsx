51ef3e4d818c33469ffe331ca62efc135c13e358
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-12-03 09:00:49 +0530

    switched resume url

--- src/components/ResumeButton.tsx
+++ src/components/ResumeButton.tsx
@@ -7,8 +7,9 @@ import { FaFileDownload } from "react-icons/fa";
 const ResumeButton: React.FC = () => {
   return (
     <a
-      href="/Piyush_Choudhari_Resume.pdf"
-      download
+      href="https://drive.google.com/file/d/1_U41v8H_fg-BrLbhr7ozEU1JSR6-bAG6/view?usp=sharing" 
+      target="_blank"
+      rel="noopener noreferrer"
       className="flex items-center bg-[#111111] hover:bg-white text-white hover:text-[#111111] px-3 py-1 rounded transition duration-300 mr-4 border border-white"
     >
       <FaFileDownload size={18} className="mr-2" />
