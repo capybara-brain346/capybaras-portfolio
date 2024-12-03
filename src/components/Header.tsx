8b6fc7fb55b84a4503fda3bcccd8d8b6eaa8c5a0
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-12-03 21:51:21 +0530

    added bg in profile pic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import GlowingProfile from "./GlowingProfile";
 
 const Header: React.FC = () => {
   return (
-    <header>
+    <header className="bg-gray-100 bg-opacity-10 p-8 rounded-lg">
       <GlowingProfile />
       <h1 className="text-4xl font-bold mb-2">Piyush Choudhari</h1>
       <h2 className="text-xl text-gray-400 mb-4">Developer & Data Analyst</h2>
